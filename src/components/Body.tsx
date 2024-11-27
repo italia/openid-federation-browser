085da3cb4db1f47bfeff7b4b649ffad0976c382c
PascalDR <akira.zeta@gmail.com>
2024-11-27 17:36:11 +0100

    feat: implemented Body component

---
+++ src/components/Body.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { InputAtom } from '../atoms/Input';
+import { TrstAnchorListAtom } from '../atoms/TrustAnchorList';
+import { isValidUrl } from "../lib/utils";
+import { GraphViewAtom } from '../atoms/GraphView';
+
+
+export const BodyComponent = () => {
+    const conponents = {
+        InputAtom: <InputAtom validationFn={isValidUrl} />,
+        TrstAnchorListAtom: <TrstAnchorListAtom />,
+        GraphView: <GraphViewAtom />
+    };
+
+    const [searchParams] = useSearchParams();
+    const [visualizedAtom, setVisualizedAtom] = useState<JSX.Element>(conponents["InputAtom"]);
+
+    useEffect(() => {
+        if(searchParams.has('listUrl')){
+            setVisualizedAtom(conponents["TrstAnchorListAtom"]);
+        } else if (searchParams.has('trustAnchorUrl')) {
+            setVisualizedAtom(conponents["GraphView"]);
+        }else{
+            setVisualizedAtom(conponents["InputAtom"]);
+        }
+    }, [searchParams]);
+
+    return (
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+            {visualizedAtom}
+        </div>
+    );
+}
