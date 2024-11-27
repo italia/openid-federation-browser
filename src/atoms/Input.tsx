cb5cebb908eb337f5cef3139092736c5b5f166c9
PascalDR <akira.zeta@gmail.com>
2024-11-27 17:31:55 +0100

    feat: Input refactoring

--- src/atoms/Input.tsx
+++ src/atoms/Input.tsx
@@ -1,30 +1,24 @@
 import { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useSearchParams } from 'react-router-dom';
-import { getCollapsable, cleanInput, handleCollapseVisibility } from '../lib/utils';
+import { getCollapsable, cleanInput } from '../lib/utils';
 
 interface InputProps {
     validationFn: (value: string) => boolean;
-    inputLabel: string;
-    invalidMessageId: string;
-    isVisible: boolean;
-}
+};
 
-export const InputAtom = ({validationFn, inputLabel, invalidMessageId, isVisible}: InputProps) => {
+export const InputAtom = ({validationFn}: InputProps) => {
     const [inputValue, setInputValue] = useState('');
     const [doCheck, setDoCheck] = useState(false);
     const [searchParams, setSearchParams] = useSearchParams();
 
     const changeValue = (e: any) => setInputValue(e.target.value);
 
-    useEffect(() => getCollapsable('invalid-input-collapse').hide(), []);
-
     useEffect(() => {
         if (!doCheck) return;
         if (validationFn(inputValue)) {
             setSearchParams({ trustAnchorUrl: inputValue });
             getCollapsable('invalid-input-collapse').hide();
-            getCollapsable('input-collapse').hide();
             cleanInput('input-value');
         }else{
             getCollapsable('invalid-input-collapse').show();
@@ -32,18 +26,19 @@ export const InputAtom = ({validationFn, inputLabel, invalidMessageId, isVisible
         setDoCheck(false);
     }, [doCheck]);
 
-    useEffect(() => handleCollapseVisibility('input-collapse', isVisible), [isVisible]);
-
     return (
-        <div className="container collapse" id="input-collapse">
+        <div style={{width: "40%", marginTop: "5%"}}>
+            <div className='row'>
+                <h4><FormattedMessage id="insert_trust_node_url_label" /></h4>
+            </div>
             <div className="row">
-                <label className="col-2" htmlFor="input-value">{inputLabel}</label>
+                <label className="col-2" htmlFor="input-value"><FormattedMessage id="url_input_label" /></label>
                 <div className="col-9"><input type="text" className="form-control" id="input-value" onChange={changeValue} /></div>
             </div>
             <div className="collapse row" id="invalid-input-collapse">
                 <div className="col-12">
                     <div className="alert alert-danger alert-dismissible fade show" role="alert">
-                        <FormattedMessage id={invalidMessageId} />
+                        <FormattedMessage id="invalid_url_error_message" />
                     </div>
                 </div>
             </div>
