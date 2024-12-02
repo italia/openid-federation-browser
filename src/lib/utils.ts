7a1d64b0135ee734d7e5f8c20d9be489e7bd5ba9
PascalDR <akira.zeta@gmail.com>
2024-12-02 11:22:57 +0100

    fix: handleCollapseVisibility function and removed unused functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,22 +9,23 @@ export const isValidUrl = (url: string): boolean => {
     }
 };
 
-export const getCollapsable = (id: string) => {
-    return new Collapse(
-        document.getElementById(id) as HTMLElement,
-        { toggle: false }
-    );
-};
-
 export const cleanInput = (id: string) => {
     const trustAnchorElm = document.getElementById(id) as HTMLInputElement;
     trustAnchorElm.value = '';
     trustAnchorElm.focus();
 };
 
-export const toggleCollapse = (id: string) => getCollapsable(id).toggle();
-
 export const handleCollapseVisibility = (id: string, isVisible: boolean) => {
-    if(isVisible) getCollapsable(id).show();
-    else getCollapsable(id).hide();
+    const collapsable = document.getElementById(id);
+
+    if(!collapsable) return;
+
+    if(!isVisible) {
+        collapsable.classList.remove('show');
+        collapsable.classList.add('hide');
+    }
+    else {
+        collapsable.classList.remove('hide');
+        collapsable.classList.add('show');
+    }
 }
