2c196ca4808e717aa38a6b4664f820377de4556d
PascalDR <akira.zeta@gmail.com>
2024-12-03 18:08:14 +0100

    feat: using new components

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { InputAtom } from '../atoms/Input';
-import { TrstAnchorListAtom } from '../atoms/TrustAnchorList';
+import { PaginatedListAtom } from '../atoms/PaginatedList';
+import { Link } from 'react-router-dom';
 import { isValidUrl } from "../lib/utils";
-import { GraphViewAtom } from '../atoms/GraphView';
+import { GraphViewComponent } from './GraphView';
 import styles from '../css/BodyComponent.module.css';
+import trustChainList from '../assets/trustChainList.json';
 
+const ItemsRenderer = ({ items }: { items: any[] }) => {
+    return (
+        <>
+        {items && items.map((d) => <li key={d.url}><Link to={`/?trustAnchorUrl=${d.url}`}>{d.name} - {d.url}</Link></li>)}
+        </>
+    );
+};
+
+const trustAnchorFilter = (anchor: any, filterValue: string) => anchor.name.toLowerCase().includes(filterValue.toLowerCase());
 
 export const BodyComponent = () => {
     const conponents = {
         InputAtom: <InputAtom validationFn={isValidUrl} />,
-        TrstAnchorListAtom: <TrstAnchorListAtom />,
-        GraphView: <GraphViewAtom />
+        TrstAnchorListAtom: <PaginatedListAtom itemsPerPage={5} items={trustChainList} ItemsRenderer={ItemsRenderer} filterFn={trustAnchorFilter}/>,
+        GraphView: <GraphViewComponent />
     };
 
     const [searchParams] = useSearchParams();
