e3c157fab21e6c3c19a278669f74db6c63648866
PascalDR <akira.zeta@gmail.com>
2024-12-11 17:57:07 +0100

    fix: data model fix

--- src/atoms/InfoView.tsx
+++ src/atoms/InfoView.tsx
@@ -1,16 +1,17 @@
 import { FormattedMessage } from 'react-intl';
+import { GraphNode } from '../lib/grap-data/types';
 
 export interface InfoViewProps {
-    data: any;
+    data: GraphNode;
 };
 
 export const InfoView = ({data}: InfoViewProps) => {
     const displayedInfo = [
         <><FormattedMessage id="federation_entity_type_label" />: {data.info.type}</>,
-        <><FormattedMessage id="immediate_subordinate_count_label" />: {data.info.dependantsLen}</>,
-        <><FormattedMessage id="status_label" />: {data.info.tree.data.ec.valid ? "valid" : "invalid"}</>,
-        <><FormattedMessage id="expired_label" />: {data.info.tree.data.ec.expired ? "expired" : "not expired"}</>,
-        <><FormattedMessage id="expiring_date_label" />: {new Date(data.info.tree.data.ec.payload.exp * 1000).toISOString()}</>
+        <><FormattedMessage id="immediate_subordinate_count_label" />: {data.info.immDependants.length}</>,
+        <><FormattedMessage id="status_label" />: {data.info.ec.valid ? "valid" : "invalid"}</>,
+        <><FormattedMessage id="expired_label" />: {data.info.ec.expired ? "expired" : "not expired"}</>,
+        <><FormattedMessage id="expiring_date_label" />: {new Date(data.info.ec.payload.exp * 1000).toISOString()}</>
     ];
 
     const toRow = (info: JSX.Element) => 
