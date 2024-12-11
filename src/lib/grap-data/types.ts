f634c7a5ebac4de8bfc5619273e07c352092d7a4
PascalDR <akira.zeta@gmail.com>
2024-12-11 17:53:55 +0100

    feat: refactored graph-data lib

--- src/lib/grap-data/types.ts
+++ src/lib/grap-data/types.ts
@@ -1,40 +1,31 @@
-import { NodeInfo } from "../openid-federation/types";
-import { Tree } from '@easygrating/easytree';
+import { 
+    SubordianteStatement, 
+    NodeInfo
+} from "../openid-federation/types";
 
-export interface Node {
+export interface GraphNode {
     id: string;
     label: string;
-    info: {
-        type: string;
-        dependantsLen: number;
-        tree: Tree<NodeInfo>;
-    };
+    info: NodeInfo;
 };
 
-export interface Edge {
+export interface GraphEdge {
     id: string;
     source: string;
     target: string;
     label: string;
+    subStatement?: SubordianteStatement;
 };
 
 export interface Graph {
-    nodes: Node[];
-    edges: Edge[];
+    nodes: GraphNode[];
+    edges: GraphEdge[];
 };
 
-export const NodeColor = {
+export const EntityColor = {
     "Trust Anchor": "#FF6347",
     "Intermediate": "#1E90FF",
     "Leaf": "#CBDFAC",
     "StartNode": "#FF8C00",
     "Undiscovered": "#696969"
-};
-
-export enum NodeType {
-    Anchor = "Trust Anchor",
-    Intermediate = "Intermediate",
-    Leaf = "Leaf",
-    StartNode = "StartNode",
-    Undiscovered = "Undiscovered",
 };
--- src/lib/grap-data/utils.ts
+++ src/lib/grap-data/utils.ts
@@ -1,26 +1,49 @@
-import { Tree } from '@easygrating/easytree';
 import { NodeInfo } from '../openid-federation/types';
-import { hasAuthorityHints, hasFetchEndpoint } from "../openid-federation/utils";
-import { NodeType } from './types';
+import { Graph, GraphNode, GraphEdge, EntityColor } from './types';
 
-export const getNodeType = (tree: Tree<NodeInfo>): NodeType => {
-    if (tree.data.startNode){
-        return NodeType.StartNode;
-    }
+export const genNode = (node: NodeInfo): GraphNode => {
+    return {
+        id: node.ec.entity, 
+        label: node.ec.entity, 
+        fill: EntityColor[node.type], 
+        info: node
+    } as GraphNode;
+};
 
-    if (!tree.data.ec.jwt){
-        return NodeType.Undiscovered;
-    }
+export const genEdge = (parent: NodeInfo, child: NodeInfo): GraphEdge => {
+    return {
+        id: `${parent.ec.entity}-${child.ec.entity}`, 
+        source: parent.ec.entity, 
+        target: child.ec.entity, 
+        label: `${parent.ec.entity}->${child.ec.entity}`, 
+        subStatement: child.ec.subordinate
+    } as GraphEdge;
+};
 
-    if (!hasFetchEndpoint(tree.data.ec.payload)){
-        return NodeType.Leaf;
-    }
+export const updateGraph = (parent: NodeInfo, child: NodeInfo, graph: Graph): Graph => {
+    const nodes = [...graph.nodes, genNode(child)];
+    const edges = [...graph.edges, genEdge(parent, child)];
 
-    if (hasAuthorityHints(tree.data.ec.payload)){
-        return NodeType.Intermediate;
-    }
+    return {nodes, edges};
+};
 
-    return NodeType.Anchor;
+export const fromNodeInfo = (node: NodeInfo): Graph => {
+    return {nodes: [genNode(node)], edges: []};
+};
+
+export const removeSubGraph = (graph: Graph, id: string): Graph => {
+    const nodes = graph.nodes.filter(node => node.id !== id);
+    const edges = graph.edges.filter(edge => edge.source !== id || edge.target !== id);
+
+    console.log(nodes, edges);
+
+    const filteredGraph = 
+        graph.edges.filter(edge => edge.source === id)
+            .map(edge => edge.target)
+            .reduce(
+                (acc, target) => removeSubGraph(acc, target), {nodes, edges});
+
+    return filteredGraph;
 };
 
 export const isEdge = (elm: any) => elm.hasOwnProperty("source") && elm.hasOwnProperty("target");
