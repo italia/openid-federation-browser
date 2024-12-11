0a57c64bee366488c224c89180dc81f9f37b201d
PascalDR <akira.zeta@gmail.com>
2024-12-11 18:01:05 +0100

    feat: refactoring

--- src/atoms/NodeMenu.tsx
+++ src/atoms/NodeMenu.tsx
@@ -1,73 +1,75 @@
 import { AccordionAtom } from "./Accordion";
 import { ECViewer } from "../atoms/ECViewer";
 import { InfoView } from "../atoms/InfoView";
-import { Tree } from "@easygrating/easytree";
-import { NodeInfo } from "../lib/openid-federation/types";
-import { Node as GraphNode } from "../lib/grap-data/types";
-import { discoverChild } from "../lib/openid-federation/trustChain";
-import { getTreeRoot } from "../lib/openid-federation/utils";
+import { GraphNode, Graph } from "../lib/grap-data/types";
+import { removeSubGraph } from "../lib/grap-data/utils";
+import { discoverChild, discoverMultipleChildren } from "../lib/openid-federation/trustChain";
 import { NoImmSubAtom } from "../atoms/NoImmSub";
 import { PaginatedListAtom } from "../atoms/PaginatedList";
+import { SubListItemsRenderer } from "./SubListItemRender";
 
 export interface ContextMenuProps {
     data: GraphNode;
-    onUpdate: (tree: Tree<NodeInfo>) => void;
+    graph: Graph;
+    onUpdate: (tree: Graph) => void;
     onError: (error: Error) => void;
 };
 
-export const NodeMenuAtom = ({data, onUpdate, onError}: ContextMenuProps) => {
+export const NodeMenuAtom = ({data, graph, onUpdate, onError}: ContextMenuProps) => {
 
-    const discoverSubordinate = (entityID: string) => {
-        const parent = data.info.tree;
-    
-        discoverChild(entityID, parent).then(tree => {
-            const root = getTreeRoot(tree); 
-            root.addChildAt(tree, parent.id)
-            onUpdate(root);
-        }).catch(onError);
-        
-        return undefined;
+    const addSubordinate = (entityID: string) =>
+        discoverChild(entityID, data.info, graph).then(onUpdate).catch(onError);
+
+    const addAllFilteredSubordinates = (items: string[]) =>
+        discoverMultipleChildren(items, data.info, graph).then(onUpdate).catch(onError);
+
+    const removeSubordinate = (entityID: string) => {
+        const newGraph = removeSubGraph(graph, entityID);
+        onUpdate(newGraph);
     };
-
-    const ItemsRenderer = (discoverSubordinate: Function, tree: Tree<NodeInfo>) => ({ items }: { items: any[] }) => {
-        const notDiscovered = (dep: string) => tree.children.find(child => child.id === dep) ? true : false;
-
-        return (
-            <>
-            {items && items.map(
-                (dep) => 
-                    <li key={dep} className="it-list-item pt-2" style={{width: "auto", height: "auto"}}>
-                        <div className="row justify-content-md-start">
-                            <div className='col-md-10'><small style={{whiteSpace: "nowrap"}}>{dep}</small></div>
-                            <div className='col-md-2'>
-                                <button className="btn btn-primary btn-icon" style={{padding: "10% 24%"}} title="Discovery" aria-label="Discovery" onClick={() => discoverSubordinate(dep)} disabled={notDiscovered(dep)}>
-                                    <span style={{fontSize: "60%"}}>Discovery</span>
-                                </button>
-                            </div>
-                        </div>
-                    </li>
-                )}
-            </>
-        );
-    }
     
     const immediateFilter = (anchor: string, filterValue: string) => anchor.toLowerCase().includes(filterValue.toLowerCase());
 
-    const InitializedRenderer = ItemsRenderer(discoverSubordinate, data.info.tree);
-
     return (
-        <div className="row">
-            <div className="accordion" id="collapseExample">
-                <AccordionAtom accordinId="info-details" labelId="node_info"  hiddenElement={<InfoView data={data} />} />
-                <AccordionAtom accordinId="immediate-subordinates-list" labelId="subordinate_list" hiddenElement={
-                    data.info.tree.data.immDependants.length === 0 
-                        ? <NoImmSubAtom /> 
-                        : <PaginatedListAtom items={data.info.tree.data.immDependants} itemsPerPage={5} ItemsRenderer={InitializedRenderer} filterFn={immediateFilter} />
-                } />
-                <AccordionAtom accordinId="entity-configuration" labelId="entity_configuration_data" hiddenElement={
-                    <ECViewer raw={data.info.tree.data.ec.jwt} decoded={data.info.tree.data.ec.payload as any} />
-                } />
+        <>
+            <div className="row">
+                <div className="accordion">
+                    <AccordionAtom 
+                        accordinId="info-details" 
+                        labelId="node_info"  
+                        hiddenElement={<InfoView data={data} />} 
+                    />
+                    <AccordionAtom 
+                        accordinId="immediate-subordinates-list" 
+                        labelId="subordinate_list" 
+                        hiddenElement={
+                            data.info.immDependants.length === 0 
+                                ? <NoImmSubAtom /> 
+                                : <PaginatedListAtom 
+                                    items={data.info.immDependants} 
+                                    itemsPerPage={5} 
+                                    ItemsRenderer={
+                                        SubListItemsRenderer(
+                                            {
+                                                graph, 
+                                                addAllFilteredSubordinates, 
+                                                addSubordinate, 
+                                                removeSubordinate
+                                            }
+                                        )
+                                    } 
+                                    filterFn={immediateFilter} />
+                        } 
+                    />
+                    <AccordionAtom 
+                        accordinId="entity-configuration" 
+                        labelId="entity_configuration_data" 
+                        hiddenElement={
+                            <ECViewer raw={data.info.ec.jwt} decoded={data.info.ec.payload as any} />
+                        } 
+                    />
+                </div>
             </div>
-        </div>
+        </>
     )
 };
