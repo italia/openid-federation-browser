526cd12385143d71fd3a71df57c33bfce2fab87b
PascalDR <akira.zeta@gmail.com>
2024-12-19 18:25:04 +0100

    feat: added warning modal

--- src/components/GraphView.tsx
+++ src/components/GraphView.tsx
@@ -1,19 +1,15 @@
-
+import { useEffect, useState} from "react";
 import { useSearchParams } from "react-router-dom";
 import { traverseUp, discovery } from "../lib/openid-federation/trustChain";
 import { GraphCanvas } from "reagraph";
 import { GraphNode, GraphEdge, Graph } from "../lib/grap-data/types";
 import { ContextMenuComponent } from "./ContextMenu";
 import { LoadingAtom } from "../atoms/Loading";
-import { ErrorViewAtom } from "../atoms/ErrorView";
 import styles from '../css/BodyComponent.module.css';
 import { fromNodeInfo } from "../lib/grap-data/utils";
-import { 
-    FC, 
-    useEffect, 
-    CSSProperties, 
-    useState,
-} from "react";
+import { WarningModalAtom } from "../atoms/WarningModal";
+import { ErrorViewAtom } from "../atoms/ErrorView";
+import { toggleModal } from "../lib/utils";
 
 enum ShowElement {
     Loading = 'loading-atom',
@@ -21,7 +17,7 @@ enum ShowElement {
     Graph = 'graph-atom'
 }
 
-export const GraphViewComponent: FC<{ style?: CSSProperties }> = ({ style }) => {
+export const GraphViewComponent = () => {
     const [searchParams] = useSearchParams();
     const [nodes, setNodes] = useState<GraphNode[]>([]);
     const [edges, setEdges] = useState<GraphEdge[]>([]);
@@ -35,7 +31,13 @@ export const GraphViewComponent: FC<{ style?: CSSProperties }> = ({ style }) => 
         setShowElement(ShowElement.Graph);
     };
 
-    const setErrorMessage = (e: Error) => {
+    const onErrorModal = (error: Error) => {
+        console.error(error);
+        setError(error);
+        toggleModal('error-modal');
+    }
+
+    const showErrorMessage = (e: Error) => {
         console.error(e);
         setError(e);
         setShowElement(ShowElement.Error);
@@ -50,37 +52,48 @@ export const GraphViewComponent: FC<{ style?: CSSProperties }> = ({ style }) => 
 
         const entityUrl = searchParams.get('trustAnchorUrl') as string;
 
-        const discoveryTree = 
+        const discoveryGraph = 
             searchParams.get("discoveryType") === "entity" 
                 ? traverseUp(entityUrl) 
                 : discovery(entityUrl).then(fromNodeInfo);
 
-        discoveryTree
+        discoveryGraph
             .then(updateGraph)
-            .catch(setErrorMessage);
+            .catch(showErrorMessage);
 
     }, [searchParams]);
 
     return (
-        <div className={styles.graphAtom}>
-            {
-                showElement === ShowElement.Loading 
-                    ? <LoadingAtom /> 
-                    : showElement === ShowElement.Error 
-                        ? <ErrorViewAtom error={error} /> 
-                        : <GraphCanvas 
-                            nodes={nodes} 
-                            edges={edges}
-                            draggable
-                            contextMenu={({ data, onClose }) => (
-                                <ContextMenuComponent 
-                                    data={data as any} 
-                                    graph={{nodes, edges}} 
-                                    onClose={onClose} 
-                                    onUpdate={onUpdate} 
-                                    onError={setErrorMessage} />
-                            )} />
-            }
-        </div>
+        <>
+            <WarningModalAtom 
+                modalID='error-modal'
+                headerID='error_modal_title'
+                description={error.message}
+                dismissActionID='modal_cancel'
+                acceptActionID='modal_confirm' 
+            />
+            <div className={styles.graphAtom}>
+                {
+                    showElement === ShowElement.Loading 
+                        ? <LoadingAtom /> 
+                        : showElement === ShowElement.Error 
+                            ? <ErrorViewAtom error={error} /> 
+                            : <GraphCanvas 
+                                nodes={nodes} 
+                                edges={edges}
+                                draggable
+                                contextMenu={({ data, onClose }) => (
+                                    <ContextMenuComponent 
+                                        data={data as any} 
+                                        graph={{nodes, edges}} 
+                                        onClose={onClose} 
+                                        onUpdate={onUpdate} 
+                                        onError={onErrorModal}
+                                    />
+                                )} 
+                              />
+                }
+            </div>
+        </>
     );
 };
