431bb00d69b2cba73803accb61d0e65c24a42da8
PascalDR <akira.zeta@gmail.com>
2024-12-19 18:22:49 +0100

    feat: added utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { Modal } from "bootstrap-italia";
+
 export const isValidUrl = (url: string): boolean => {
     try {
         new URL(url);
@@ -37,4 +39,22 @@ export const handleKeyDwonEvent = (key: string, onEvent: () => void) => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
+};
+
+export const toggleModal = (id: string) => {
+    const modal = new Modal(document.getElementById(id) as HTMLElement);
+    modal.toggle();
+};
+
+export const fmtValidity = (valid: boolean, reason: string | undefined) => {
+    const value = valid ? "valid" : "invalid";
+    return valid ? value : `${value} (${reason})`;
+};
+
+export const truncateMiddle = (text: string, maxLength: number) => {
+    if (text.length <= maxLength) {
+        return text;
+    }
+    const half = Math.floor(maxLength / 2);
+    return text.slice(0, half) + '...' + text.slice(-half);
 };
