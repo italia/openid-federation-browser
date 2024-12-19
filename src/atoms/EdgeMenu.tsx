e39eb782a8e0503be42dfec1a445a910cad5c78b
PascalDR <akira.zeta@gmail.com>
2024-12-19 18:20:40 +0100

    feat: added info

--- src/atoms/EdgeMenu.tsx
+++ src/atoms/EdgeMenu.tsx
@@ -1,6 +1,8 @@
 import { GraphEdge } from "../lib/grap-data/types";
 import { AccordionAtom } from "./Accordion";
 import { JWTViewer } from "./JWTViewer";
+import { fmtValidity } from "../lib/utils";
+import { InfoView } from "./InfoView";
 
 export interface EdgeMenuAtomProps {
     data: GraphEdge;
@@ -12,18 +14,38 @@ export const EdgeMenuAtom = ({data}: EdgeMenuAtomProps) => {
             <div className="accordion">
                 {
                     data.subStatement &&
-                    <AccordionAtom 
-                        accordinId="subordinate-statement" 
-                        labelId="entity_configuration_data" 
-                        hiddenElement={
-                            <JWTViewer 
-                                id="edge-viewer" 
-                                raw={data.subStatement.jwt} 
-                                decoded={data.subStatement.payload as any} />
-                        } 
-                    />
+                    <>
+                        <AccordionAtom 
+                            accordinId="info-details" 
+                            labelId="node_info"  
+                            hiddenElement={
+                                <InfoView 
+                                    id={`${data.label}-view`} 
+                                    infos={[
+                                        [
+                                            "status_label",  
+                                            fmtValidity(data.subStatement.valid, data.subStatement.invalidReason)
+                                        ],
+                                        [
+                                            "expiring_date_label", 
+                                            new Date(data.subStatement.payload.exp * 1000).toLocaleString()
+                                        ]
+                                    ]} />} 
+                        />
+                        <AccordionAtom 
+                            accordinId="subordinate-statement" 
+                            labelId="entity_configuration_data" 
+                            hiddenElement={
+                                <JWTViewer 
+                                    id="edge-viewer" 
+                                    raw={data.subStatement.jwt} 
+                                    decoded={data.subStatement.payload as any} />
+                            } 
+                        />
+                    </>
+                    
                 }
             </div>
         </div>
-    )
-}
+    );
+};
