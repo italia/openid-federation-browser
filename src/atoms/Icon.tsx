a449302740ce46bede7c7c59aaa49db0233f9248
PascalDR <akira.zeta@gmail.com>
2025-01-22 12:30:05 +0100

    feat: icon custom style

--- src/atoms/Icon.tsx
+++ src/atoms/Icon.tsx
@@ -4,12 +4,15 @@ interface IconProps {
   iconID: string;
   className?: string;
   isRounded?: boolean;
+  style?: React.CSSProperties
 }
 
-export const IconAtom = ({ iconID, className, isRounded }: IconProps) => {
+export const IconAtom = ({ iconID, className, isRounded, style }: IconProps) => {
+  const currentStyle = style ? style : {};
+
   return (
     <span className={isRounded ? "rounded-icon" : ""}>
-      <svg className={`icon ${className ? className.toString() : ""}`}>
+      <svg className={`icon ${className ? className.toString() : ""}`} style={{...currentStyle}}>
         <use xlinkHref={sprite + iconID}></use>
       </svg>
     </span>
