837324933e91c08afacf602ed8eabc9f1090babe
PascalDR <akira.zeta@gmail.com>
2025-01-31 16:13:33 +0100

    fix: build directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  build: {
+    outDir: "build",
+  },
   plugins: [
     react(),
     checker({
