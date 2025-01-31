8580a94f874a8554928b3aea893ccda74d316fed
PascalDR <akira.zeta@gmail.com>
2025-01-31 15:43:09 +0100

    fix: MetaProperty warning

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router basename={import.meta.env.VITE_BASENAME}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
