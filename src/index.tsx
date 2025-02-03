3649d93f494ed148772b929de2ea0d84bef86616
PascalDR <akira.zeta@gmail.com>
2025-02-03 15:22:11 +0100

    fix: minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router basename={import.meta.env.VITE_BASE_PATH || "./"}>
+    <Router basename={import.meta.env.VITE_BASE_PATH}>
       <App />
     </Router>
   </React.StrictMode>,
