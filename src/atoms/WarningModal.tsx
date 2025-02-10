8ce13c0dc80922de58fd6ed39fbb3eaa50afbef4
PascalDR <akira.zeta@gmail.com>
2025-02-10 16:21:52 +0100

    fix: warning z-index

--- src/atoms/WarningModal.tsx
+++ src/atoms/WarningModal.tsx
@@ -27,7 +27,7 @@ export const WarningModalAtom = ({
   onDismiss,
 }: WarningModalProps) => {
   return (
-    <div className="modal" tabIndex={-1} role="dialog" id={modalID}>
+    <div className="modal" tabIndex={-1} role="dialog" id={modalID} style={{zIndex: 16169932}}>
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
