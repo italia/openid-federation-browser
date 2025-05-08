5fb2252471b3b55cd86cf242873ff99968a58d82
PascalDR <akira.zeta@gmail.com>
2025-05-08 17:41:34 +0200

    test: added more steps to test

--- cypress/e2e/context-sidebar.cy.ts
+++ cypress/e2e/context-sidebar.cy.ts
@@ -114,5 +114,95 @@ describe('ContextSideBar spec', () => {
 
     cy.wait(1000);
 
+    //check that entities are listed and visible
+    cy.get('[data-testid="entity-item"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.length', 5);
+
+    cy.get('[data-testid="add-remove-entities-button"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-success')
+      .should('have.length', 5);
+
+    cy.get('[data-testid="highlight-entities-button"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-primary')
+      .should('have.length', 5)
+      .should('be.disabled');
+
+    //check that buttons are correctly displayed
+    cy.get('#add-all-entities-button')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-primary')
+      .contains('Add all this 5 in page');
+
+    cy.get('#remove-all-entities-button')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-danger')
+      .contains('Remove All');
+
+    cy.get('#add-filtered-entities-button')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-outline-primary')
+      .contains('Add all filtered');
+
+    // check that the nodes are discovered
+    cy.get('#add-all-entities-button')
+      .click();
+
+    cy.wait(1000);
+
+    cy.get('[data-testid="warning-modal"]')
+      .should('exist')
+      .should('be.visible');
+
+    cy.get('[data-testid="warning-modal-dismiss-button"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-outline-primary btn-sm')
+      .contains('Cancel');
+
+    cy.get('[data-testid="warning-modal-accept-button"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-primary btn-sm')
+      .contains('Continue')
+      .click();
+
+    cy.get('[data-testid="warning-modal"]')
+      .should('be.not.visible');
+
+    cy.wait(5000);
+
+    cy.get('#add-all-entities-button')
+      .should('exist')
+      .should('be.visible')
+      .should('be.disabled')
+      .contains('Add all this 0 in page');
+
+    cy.get('[data-testid="add-remove-entities-button"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-danger')
+      .should('have.length', 5);
+
+    cy.get('[data-testid="highlight-entities-button"]')
+      .should('exist')
+      .should('be.visible')
+      .should('have.class', 'btn btn-sm py-0 px-1 btn-primary')
+      .should('have.length', 5)
+      .should('be.not.disabled');
+
+    cy.get('[data-testid="highlight-entities-button"]')
+      .first()
+      .click();
+
+    cy.wait(1000);
   });
 });
