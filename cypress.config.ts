e986a8422898465bd02e68ecf7b6ec11d305262e
PascalDR <akira.zeta@gmail.com>
2025-05-13 15:41:07 +0200

    fix: disable gpu

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       on('before:browser:launch', (browser: Cypress.Browser, launchOptions) => {
         if (browser.name === 'electron') {
           launchOptions.preferences.webPreferences.webSecurity = false;
+          launchOptions.args.push('--disable-gpu'); // Disable GPU acceleration
   
           return launchOptions;
         }
