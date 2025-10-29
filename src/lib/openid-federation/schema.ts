ab6f51a53947813a93e70710ce5bc9cf9f50b461
Marche271 <marchesini.eleonora@gmail.com>
2025-10-29 15:12:01 +0100

    feat: add support for schema validation payload (RP) and header (ALL)

--- src/lib/openid-federation/schema.ts
+++ src/lib/openid-federation/schema.ts
@@ -1,6 +1,8 @@
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
+import headerEntityConfiguration from "./schemas/headerEntityConfiguration.schema.json";
 import entityConfiguration from "./schemas/entityConfiguration.schema.json";
+import leafEntityConfiguration from "./schemas/leafEntityConfiguration.schema.json";
 import subordinateStatement from "./schemas/subordinateStatement.schema.json";
 import headerTrustMark from "./schemas/headerTrustMark.schema.json";
 import trustMark from "./schemas/trustMark.schema.json";
@@ -13,10 +15,24 @@ const avj = new Ajv({
 
 addFormats(avj);
 
+const headerEntityConfigurationValidator = avj.compile(headerEntityConfiguration);
 const entityConfigurationValidator = avj.compile(entityConfiguration);
+const leafEntityConfigurationValidator = avj.compile(leafEntityConfiguration);
 const subordinateStatementValidator = avj.compile(subordinateStatement);
 const headerTrustMarkValidator = avj.compile(headerTrustMark);
 const trustMarkValidator = avj.compile(trustMark);
+
+export const validateHeaderEntityConfiguration = async (
+  header: object
+): Promise<[boolean, string | undefined]> => {
+  const valid = await headerEntityConfigurationValidator(header);
+
+  if (valid) {
+    return [true, undefined];
+  }
+
+  return [false, avj.errorsText(headerEntityConfigurationValidator.errors)];
+};
 
 export const validateEntityConfiguration = async (
   ec: object,
@@ -28,6 +44,18 @@ export const validateEntityConfiguration = async (
   }
 
   return [false, avj.errorsText(entityConfigurationValidator.errors)];
+};
+
+export const validateLeafEntityConfiguration = async (
+  payload: object
+): Promise<[boolean, string | undefined]> => {
+  const valid = await leafEntityConfigurationValidator(payload);
+
+  if (valid) {
+    return [true, undefined];
+  }
+
+  return [false, avj.errorsText(leafEntityConfigurationValidator.errors)];
 };
 
 export const validateHeaderTrustMark = async (
